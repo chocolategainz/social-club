6f5ada3db676cf069c3fa35548cad5dda2b98cae
Aaron Thomas <05moreminutes@gmail.com>
2025-02-27 01:13:26 +0000

    adding more pictures to the web page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import Image from 'next/image';
-import "./globals.css";
+
+
 export default function Home() {
   return(
   <div className="home">
   
     <div className="main-picture relative w-full h-[932px]">
 <Image 
-src="/zachary-nelson-98Elr-LIvD8-unsplash.jpg"
+src="/johan-mouchet-MCTap3U0bFg-unsplash.jpg"
     alt="friends jumping"
     fill
     style={{objectFit: "cover"}}
@@ -15,33 +16,52 @@ src="/zachary-nelson-98Elr-LIvD8-unsplash.jpg"
     />
     </div>
 
-<div className="second-layer w-full h-[820px] bg-[#880808]">
+<div className="second-layer">
 <Image
 src="/elevate-snnhGYNqm44-unsplash.jpg"
 alt="friends drinking beer"
 loading="lazy"
-width= {460}
-height= {460}
+width= {420}
+height= {420}
 className="rounded-full object-cover aspect-square"
 />
 
 <Image
-src="/tim-mossholder-hOF1bWoet_Q-unsplash.jpg"
+src="/yingyi-dai-khJJqciKS10-unsplash.jpg"
 alt="group of friends together smiling"
 loading="lazy"
-width= {460}
-height= {460}
+width= {420}
+height= {420}
 className="rounded-full object-cover aspect-square"
+id="smiles"
 />
 
 <Image
 src="/Together We Are Stronger.jpg"
-alt="paper"
+alt="inspirational paper message"
 loading="lazy"
-width= {460}
-height= {460}
+width= {420}
+height= {420}
 className="rounded-full object-cover aspect-square"
 />
+</div>
+
+<div className="secondaryPicture relative w-full h-[600px] aspect-[16/9]">
+  <Image 
+    src="/kobby-mendez-xBFTjrMIC0c-unsplash.jpg"
+    alt="Beautiful lights with young adults raising their hands"
+    fill
+    style = {{objectFit: "cover"}}
+    loading="lazy"
+    className='saturate-50'
+  />
+</div>
+
+<div className = "footer">
+  <h5>Location</h5>
+  <h1> Demo Street</h1>
+  <h1>London, E12 345,</h1>
+  <h1> United Kingdom</h1>
 </div>
  </div>
   ) 
