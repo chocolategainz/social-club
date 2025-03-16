1e3d901dc7a28a40e3c56bb0f5613c5bdfd40b14
Aaron Thomas <05moreminutes@gmail.com>
2025-03-16 00:21:14 +0000

    ...

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return(
   <div className="home">
 
-    <div className="main-picture relative w-full sm:max-w-full flex items-center justify-center"> 
+    <div className="main-picture relative w-full min-h-screen flex items-center justify-center"> 
 <Image 
 src="/johan-mouchet-MCTap3U0bFg-unsplash.jpg"
     alt="friends sitting down and talking"
