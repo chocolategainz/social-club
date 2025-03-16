5cea94906c0d06a744c3ff1077393e2484f0e20f
Aaron Thomas <05moreminutes@gmail.com>
2025-03-16 02:22:53 +0000

    ...

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return(
   <div className="home">
 
-<div className="parent h-screen ">
+<div className="parent h-screen overflow-hidden">
     <div className="main-picture relative w-full h-full flex items-center justify-center"> 
 <Image 
 src="/johan-mouchet-MCTap3U0bFg-unsplash.jpg"
